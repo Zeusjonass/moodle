ef0085ced1f1520a5b94eb1e213fc2af476e4656
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-11-09 13:47:42 +1300

    MDL-32880: Coding style - convert tabs to spaces in rss_client block handler

--- blocks/rss_client/backup/moodle1/lib.php
+++ blocks/rss_client/backup/moodle1/lib.php
@@ -14,14 +14,14 @@ defined('MOODLE_INTERNAL') || die();
  * Block conversion handler for rss_client
  */
 class moodle1_block_rss_client_handler extends moodle1_block_handler {
-	public function process_block(array $data) {
-		parent::process_block($data);
-		$instanceid = $data['id'];
-		$contextid = $this->converter->get_contextid(CONTEXT_BLOCK, $data['id']);
+    public function process_block(array $data) {
+        parent::process_block($data);
+        $instanceid = $data['id'];
+        $contextid = $this->converter->get_contextid(CONTEXT_BLOCK, $data['id']);
 
-		// Moodle 1.9 backups do not include sufficient data to restore feeds, so we need an empty shell rss_client.xml
-		// for the restore process to find
-		$this->open_xml_writer("course/blocks/{$data['name']}_{$instanceid}/rss_client.xml");
+        // Moodle 1.9 backups do not include sufficient data to restore feeds, so we need an empty shell rss_client.xml
+        // for the restore process to find
+        $this->open_xml_writer("course/blocks/{$data['name']}_{$instanceid}/rss_client.xml");
         $this->xmlwriter->begin_tag('block', array('id' => $instanceid, 'contextid' => $contextid, 'blockname' => 'rss_client'));
         $this->xmlwriter->begin_tag('rss_client', array('id' => $instanceid));
         $this->xmlwriter->full_tag('feeds', '');
@@ -30,5 +30,5 @@ class moodle1_block_rss_client_handler extends moodle1_block_handler {
         $this->close_xml_writer();
 
         return $data;
-	}
+    }
 }
